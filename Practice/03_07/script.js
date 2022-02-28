4a7aeb504c68515a921d8eb5b4fc0dfd4e6bfce0
Akshit Baunthiyal <akshit.baunthy@gmail.com>
2022-02-27 17:13:19 -0800

    Added class and object

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,20 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+class Backpack {
+    constructor(colour, shape) {
+        this.colour = colour;
+        this.shape = shape;
+    }
+}
+
+class Shape {
+    constructor(shapeName) {
+        this.name = shapeName;
+    }
+}
+
+const myBackpack = new Backpack("black", new Shape("cuboid"));
+
+console.log(myBackpack);
